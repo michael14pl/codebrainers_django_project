d09ce3f48944acf979068374563e940dc2a73377
Piotr Ociepka <pkociepka@gmail.com>
2019-05-14 17:05:43 +0200

    Test votes

--- wykop/posts/tests.py
+++ wykop/posts/tests.py
@@ -16,4 +16,4 @@ class BarTest(TestCase):
 		vote1 = Vote.objects.create(user=user1, post=post, value=1)
 		vote2 = Vote.objects.create(user=user2, post=post, value=1)
 		
-		assertEqual(Post.objects.get(id=1).score(), 2)
+		assertEquals(Post.objects.get(id=1).score(), 2)
