195e1aa36b433f22c61035bd11df543a9a6a3b8d
Piotr Ociepka <pkociepka@gmail.com>
2019-05-14 16:45:35 +0200

    Test votes

--- wykop/posts/tests.py
+++ wykop/posts/tests.py
@@ -1,7 +1,18 @@
 from django.test import TestCase
 
+from catalog.models import Post, Vote, User
+
 # Create your tests here.
 
 class BarTest(TestCase):
 	def test_false(self):
 		self.assertFalse(False)
+
+	def test_votes(self):
+		user1 = User.objects.create()
+		user2 = User.objects.create()
+		post = Post.objects.create()
+		vote1 = Vote.objects.create(user=user1, post=post, value=1)
+		vote2 = Vote.objects.create(user=user2, post=post, value=1)
+		
+		assertEqual(Post.objects.get(id=1).score(), 2)
